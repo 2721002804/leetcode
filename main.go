2708ab28a3c9bb36c35bdc49d41ce2eb3dbf5250
liuhui12 <liuhui12@staff.weibo.com>
2021-08-15 23:45:36 +0800

    3

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 //1. 两数之和
 func twoSum(nums []int, target int) []int {
@@ -93,12 +96,111 @@ func add(s []int) {
 	s = append(s, 1)
 	return
 }
+
+//704. 二分查找
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else if nums[mid] > target {
+			high = mid - 1
+		}
+	}
+	return -1
+}
+
+//278. 第一个错误的版本
+func firstBadVersion(n int) int {
+	low := 1
+	high := n
+	bad := []int{}
+	for low <= high {
+		mid := (low + high) / 2
+		res := isBadVersion(mid)
+		if res {
+			bad = append(bad, mid)
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	sort.Ints(bad)
+	return bad[0]
+}
+func isBadVersion(version int) bool {
+	if version == 2 {
+		return true
+	}
+	return false
+}
+
+//35. 搜索插入位置
+func searchInsert(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	mid := (low + high) / 2
+	for low <= high {
+		mid = (low + high) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else if nums[mid] > target {
+			high = mid - 1
+		}
+	}
+	if high < 0 {
+		return 0
+	}
+	if low > len(nums)-1 {
+		return len(nums)
+	}
+	return low
+}
+func BumSort(a []int) {
+	for i := 0; i < len(a)-1; i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[j] < a[i] {
+				a[j], a[i] = a[i], a[j]
+			}
+		}
+	}
+}
+func QuickSort(a []int, left, right int) {
+	if left > right {
+		return
+	}
+	i := left
+	j := right
+	flag := a[left]
+	for i < j {
+		for a[j] >= flag && i < j {
+			j--
+		}
+		for a[i] <= flag && i < j {
+			i++
+		}
+		if i < j {
+			a[i], a[j] = a[j], a[i]
+		}
+	}
+	a[i], a[left] = a[left], a[i]
+	QuickSort(a, left, j-1)
+	QuickSort(a, j+1, right)
+}
 func main() {
 	//nums := []int{2, 7, 11, 15}
 	//fmt.Println(twoSum(nums, 9))
 	//s := generateParenthesis(2)
 	//fmt.Println(s)
-	s := []int{}
-	add(s)
+	s := []int{10, 4, 7, 1, 5, 2}
+	//add(s)
+	//BumSort(s)
+	QuickSort(s, 0, len(s)-1)
 	fmt.Println(s)
 }
