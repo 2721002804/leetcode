a97a161b207f354520dc3337565587c608359058
liuhui12 <liuhui12@staff.weibo.com>
2021-08-15 18:29:54 +0800

    2

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
+//1. 两数之和
 func twoSum(nums []int, target int) []int {
 	hashTable := map[int]int{}
 	for i, x := range nums {
@@ -14,6 +13,8 @@ func twoSum(nums []int, target int) []int {
 	}
 	return nil
 }
+
+//5. 最长回文子串
 func longestPalindrome(s string) string {
 	start := 0
 	end := 0
@@ -39,6 +40,8 @@ func explo(s string, start, end int) int {
 	}
 	return end - start - 1
 }
+
+//3. 无重复字符的最长子串
 func lengthOfLongestSubstring(s string) int {
 	r := 0
 	ans := 0
@@ -61,7 +64,41 @@ func max(x, y int) int {
 	}
 	return y
 }
+
+//22. 括号生成
+func generateParenthesis(n int) []string {
+	s := []string{}
+	if n == 0 {
+		return s
+	}
+	dfs("", n, n, &s)
+	return s
+}
+func dfs(curStr string, left int, right int, res *[]string) {
+	if left == 0 && right == 0 {
+		*res = append(*res, curStr)
+		return
+	}
+	if left > right {
+		return
+	}
+	if left > 0 {
+		dfs(curStr+"(", left-1, right, res)
+	}
+	if right > 0 {
+		dfs(curStr+")", left, right-1, res)
+	}
+}
+func add(s []int) {
+	s = append(s, 1)
+	return
+}
 func main() {
-	nums := []int{2, 7, 11, 15}
-	fmt.Println(twoSum(nums, 9))
+	//nums := []int{2, 7, 11, 15}
+	//fmt.Println(twoSum(nums, 9))
+	//s := generateParenthesis(2)
+	//fmt.Println(s)
+	s := []int{}
+	add(s)
+	fmt.Println(s)
 }
